46c7b53d70196435385a91cc241bb1a3a5b9d8fb
phts1 <42389367+phts1@users.noreply.github.com>
2019-02-20 18:01:32 +0300

    Add files via upload

---
+++ code.js
@@ -0,0 +1,64 @@
+const checkbox1 = document.getElementById('Men');
+const saleman = document.getElementById('Man');
+const checkbox2 = document.getElementById('Women');
+const salewoman = document.getElementById('Woman');
+const checkbox3 = document.getElementById('Children');
+const salechildren = document.getElementById('Сhildrenn');
+const checkbox4 = document.getElementById('Accessories');
+const saleaccessories = document.getElementById('Accessoriess');
+
+const toggle = function(param) {
+        if (param.elementToDisplay !== null) {
+            if (param.checkbox && param.checkbox.checked) {
+                param.elementToDisplay.style.display = 'block';
+            } else {
+                param.elementToDisplay.style.display = 'none';
+            }
+        }
+
+}
+const checkboxes = [{
+        checkbox: checkbox1,
+        elementToDisplay: saleman
+    },
+    {
+        checkbox: checkbox2,
+        elementToDisplay: salewoman
+    },
+    {
+        checkbox: checkbox3,
+        elementToDisplay: salechildren
+    },
+    {
+        checkbox: checkbox4,
+        elementToDisplay: saleaccessories
+    },
+];
+for (let i in checkboxes) {
+    checkboxes[i].checkbox.addEventListener('change', (event) => {
+        toggle(checkboxes[i]);
+    });
+}
+
+const button1 = document.getElementById('content__button');
+button1.addEventListener('click', () => {
+    let isToggleOn = false;
+    for (let i in checkboxes) {
+        if (!checkboxes[i].checkbox.checked) {
+            isToggleOn = true;
+            break;
+        }
+    }
+    for (let i in checkboxes) {
+        checkboxes[i].checkbox.checked = isToggleOn;
+    }
+
+    toggleAll();
+});
+const toggleAll = function () {
+    for (let i in checkboxes) {
+        toggle(checkboxes[i]);
+    }
+}
+
+toggleAll();
